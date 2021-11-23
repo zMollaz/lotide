a542ea6df7e9ce2bda746d3453769dd88959b9c3
A E <aelmollaz@gmail.com>
2021-11-23 13:27:45 +0000

    Implement head

--- head.js
+++ head.js
@@ -5,8 +5,6 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
 
 const head = function(arr) {
   return arr.shift();
