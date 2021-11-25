4c4a70169564e0646cb862dcd5097c78b62445d8
A E <aelmollaz@gmail.com>
2021-11-25 00:20:55 +0000

    Added countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const results = {};
+  for (const letter of sentence) {
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  } return results;
+};
+
+//test code
+
+/*{
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+};*/
+const actual = countLetters("lighthouse in the house");
+console.log(countLetters("lighthouse in the house"));
+assertEqual(actual["h"], 4);
