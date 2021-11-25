2ba303a750883b086f911856b3fd75706e50dd1d
A E <aelmollaz@gmail.com>
2021-11-25 03:54:32 +0000

    Added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj,val) {
+  const extract = Object.keys(obj);
+  for (let key of extract) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+};
+
+//Test code
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
