d35aafa7a3f194b86501634ec6dbd9a0b4d14d62
A E <aelmollaz@gmail.com>
2021-11-30 00:59:40 +0000

    Refactored tailTest.js to use M & C

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,18 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 const words = ["Hello", "Lighthouse", "Labs"];
-const result = tail(words);
 
-assertEqual([].length, 0);
-assertEqual(result.length, 2); 
-assertEqual(words.length, 3); 
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+describe('#tail',() => {
+  it('return an empty array if input is an empty array []', () => {
+    assert.deepEqual(tail([]), []);
+  });
+  it('return ["Lighthouse", "Labs"] as the modified array', () => {
+    assert.deepEqual(tail(words), ["Lighthouse", "Labs"]);
+  });
+  it('return 2 for the length of modified array', () => {
+    assert.deepEqual(tail(words).length, 2);
+  });
+  it('return Lighthouse for index 0 of modified array', () => {
+    assert.deepEqual(tail(words)[0], "Lighthouse");
+  });
+});
