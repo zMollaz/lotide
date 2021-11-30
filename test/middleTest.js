15e934ef01560609a0d361efde2ea40218bf2521
A E <aelmollaz@gmail.com>
2021-11-30 00:56:12 +0000

    Refactored middleTest.js to use M & C

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,14 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe('#middle', () => {
+  it('return an empty array for an array that has less than 3 numbers' , () => {
+    assert.deepEqual((middle([1, 2])), []);
+  });
+  it('return [2] for [1, 2, 3]' , () => {
+    assert.deepEqual((middle([1, 2, 3])), [2]);
+  });
+  it('return [2, 3] for [1, 2, 3, 4]', () => {
+    assert.deepEqual((middle([1, 2, 3, 4])), [2, 3]);
+  });
+});
