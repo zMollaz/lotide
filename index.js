1dd37bda18d01967dcca6d88df15a1129f191c0a
A E <aelmollaz@gmail.com>
2021-11-30 02:33:44 +0000

    Refactored with Object short hand syntax

--- index.js
+++ index.js
@@ -16,20 +16,20 @@ const takeUntil          = require('./takeUntil');
 const without            = require('./without');
 
 module.exports = {
-  assertArraysEqual:  assertArraysEqual,
-  assertEqual:        assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters:       countLetters,
-  countOnly:          countOnly,
-  eqArrays:           eqArrays,
-  eqObjects:          eqObjects,
-  findKey:            findKey,
-  findKeyByValue:     findKeyByValue,
-  head:               head,
-  letterPositions:    letterPositions,
-  map:                map,
-  middle:             middle,
-  tail:               tail,
-  takeUntil:          takeUntil,
-  without:            without
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
 };
